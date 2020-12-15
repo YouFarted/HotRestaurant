21e96d318080f3498e36c9bad01848146fa5b30f
Andrew Olson <frrrkin@gmail.com>
2020-12-14 20:58:59 -0800

    register api routes

--- index.js
+++ index.js
@@ -15,10 +15,23 @@ var allReservations = [
     phone: "9999999999"}
 ]
 
+var tables = [
+    {id:"idOnTable1",
+    name:"idOnTable1",
+    email: "email",
+    phone: "1234567890"},
+    {id:"idMoop",
+    name: "idMoop",
+    email: "moop@gmail.com",
+    phone: "9999999999"}
+]
+
 app.use(express.static('public'))
 app.use(express.json())
 
 app.get( '/', (req, res) => res.sendFile(path.join(__dirname, "./public/home.html")))
+app.get( '/reserve', (req, res) => res.sendFile(path.join(__dirname, "./public/reserve.html")))
+app.get( '/tables', (req, res) => res.sendFile(path.join(__dirname, "./public/tables.html")))
 // get all the reservations IN JSON
 app.get( '/api/reservations', function(req,res) {
     //res.end(JSON.stringify(allReservations))
